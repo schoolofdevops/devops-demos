97fc14dc6f31fbf3a860eab0bf58a52c727e7b71
Vijayboopathy <vijayboopathy.e@gmail.com>
2016-11-22 04:33:20 +0530

    ELK stack automation

--- ci-01/docker-compose-installation.sh
+++ ci-01/docker-compose-installation.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 apt update
-apt install git
-wget -qO- https://get.docker.com/ | sh
-sudo usermod -aG docker $(whoami)
 apt-get -y install python-pip
-pip install docker-compose
+PKG_OK=$(pip show='${Status}\n' docker|grep "Docker-compose is already installed")
+echo Checking for Docker-compose: $PKG_OK
+if [ "" == "$PKG_OK" ]; then
+  echo "No Docker-compose found. Setting up."
+  wget -qO- https://get.docker.com/ | sh
+  sudo usermod -aG docker $(whoami)
+  pip install docker-compose
+fi
+apt install git
 cd /home/ubuntu
 git clone https://github.com/deviantony/docker-elk.git
 cd docker-elk
