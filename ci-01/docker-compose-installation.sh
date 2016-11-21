7983816b481aa03541c03a3572b9a26102148dba
Vijayboopathy <vijayboopathy.e@gmail.com>
2016-11-22 04:04:48 +0530

    ELK stack automation

--- ci-01/docker-compose-installation.sh
+++ ci-01/docker-compose-installation.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+apt update
+apt install git
 wget -qO- https://get.docker.com/ | sh
 sudo usermod -aG docker $(whoami)
 apt-get -y install python-pip
 pip install docker-compose
+cd /home/ubuntu
+git clone https://github.com/deviantony/docker-elk.git
+cd docker-elk
+sysctl -w vm.max_map_count=262144
+docker-compose up -d
