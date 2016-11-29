4e1193c49e20023fb01d8a65d67af7abade3c2d8
Vijayboopathy <vijayboopathy.e@gmail.com>
2016-11-30 00:45:21 +0530

    Swarm_Service_Create_Script

--- ci-01/image.sh
+++ ci-01/image.sh
@@ -21,8 +21,3 @@ spawn docker push initcronregistry.org/tomcatapp
 log_user 1
 expect eof
 EOF
-if docker service ls 2> /dev/null | grep -q -i "tomapp"; then
-        echo "Service is already running"
-else
-        docker service create --replicas 6 --publish 8080:8080 --log-driver=syslog  --log-opt syslog-address=tcp://192.168.0.53:5000 --name tomapp initcronregistry.org/tomcatapp
-fi
